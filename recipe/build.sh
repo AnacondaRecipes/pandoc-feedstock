5fb4366616a9d28b981dd0bb2f2e5d282375f8f2
Ray Donnelly <mingw.android@gmail.com>
2017-09-06 20:06:18 +0000

    Add linux-32 repackaging from epel-5

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-if [ $(uname) == Linux ]; then
+mkdir -p ${PREFIX}/bin
+if [[ $(uname) == Linux ]] && [[ $(uname -m) == x86_64 ]]; then
     ar vx pandoc*.deb
     tar --extract --xz --verbose --file=data.tar.xz
-    mkdir -p $PREFIX/bin
-    mv usr/bin/* $PREFIX/bin
-fi
-
-
-if [ $(uname) == Darwin ]; then
-    pkgutil --expand pandoc-$PKG_VERSION-macOS.pkg pandoc
+    mv usr/bin/* ${PREFIX}/bin/
+elif [[ $(uname) == Linux ]] && [[ $(uname -m) == i686 ]]; then
+    RPMS=$(find . -name "*.rpm")
+    for RPM in ${RPMS}; do
+        rpm2cpio ${RPM} | cpio -idv
+    done
+    mv usr/bin/* ${PREFIX}/bin/
+    for EXE in pandoc pandoc-citeproc; do
+        if [[ -f ${PREFIX}/bin/${EXE} ]]; then
+            patchelf --replace-needed libgmp.so.3 libgmp.so.10 ${PREFIX}/bin/${EXE}
+            patchelf --replace-needed libffi.so.5 libffi.so.6  ${PREFIX}/bin/${EXE}
+        fi
+    done
+elif [[ $(uname) == Darwin ]]; then
+    pkgutil --expand pandoc-${PKG_VERSION}-macOS.pkg pandoc
     cpio -i -I pandoc/pandoc.pkg/Payload
-    mkdir -p $PREFIX/bin
-    cp usr/local/bin/* $PREFIX/bin/
+    cp usr/local/bin/* ${PREFIX}/bin/
 fi
