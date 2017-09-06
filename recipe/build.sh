a0ae913048f0c1febaecfcfa9a50fac68249c8e2
Ray Donnelly <mingw.android@gmail.com>
2017-09-06 20:06:18 +0000

    Add linux-32 repackaging from epel-5

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-if [ $(uname) == Linux ]; then
+mkdir -p ${PREFIX}/bin
+if [[ $(uname) == Linux ]] && [[ $(uname -m) == x86_64 ]]; then
     ar vx pandoc*.deb
     tar -xzvf data.tar.gz
-    mkdir -p $PREFIX/bin
-    mv usr/bin/* $PREFIX/bin
-fi
-
-# TODO :: The * here is because of a conda-build bug where
-# TODO :: the hash gets added. ca-certificates suffers too
-if [ $(uname) == Darwin ]; then
-    pkgutil --expand pandoc-${PKG_VERSION}*.pkg pandoc
+    mv usr/bin/* ${PREFIX}/bin/
+elif [[ $(uname) == Linux ]] && [[ $(uname -m) == i686 ]]; then
+    RPMS=$(find . -name "*.rpm")
+    for RPM in ${RPMS}; do
+        rpm2cpio ${RPM} | cpio -idv
+    done
+    mv usr/bin/* ${PREFIX}/bin/
+    for EXE in pandoc pandoc-citeproc; do
+        patchelf --replace-needed libgmp.so.3 libgmp.so.10 ${PREFIX}/bin/${EXE}
+        patchelf --replace-needed libffi.so.5 libffi.so.6  ${PREFIX}/bin/${EXE}
+    done
+elif [[ $(uname) == Darwin ]]; then
+    pkgutil --expand pandoc-${PKG_VERSION}.pkg pandoc
     cpio -i -I pandoc/pandoc.pkg/Payload
-    mkdir -p $PREFIX/bin
-    cp usr/local/bin/* $PREFIX/bin/
+    cp usr/local/bin/* ${PREFIX}/bin/
 fi
