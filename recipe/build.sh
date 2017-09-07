814c3e72bdcc19902413de4f1c770199e94745c6
Ray Donnelly <mingw.android@gmail.com>
2017-09-07 16:03:53 +0000

    Fix pandoc for linux-32

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,8 +12,10 @@ elif [[ $(uname) == Linux ]] && [[ $(uname -m) == i686 ]]; then
     done
     mv usr/bin/* ${PREFIX}/bin/
     for EXE in pandoc pandoc-citeproc; do
-        patchelf --replace-needed libgmp.so.3 libgmp.so.10 ${PREFIX}/bin/${EXE}
-        patchelf --replace-needed libffi.so.5 libffi.so.6  ${PREFIX}/bin/${EXE}
+        if [[ -f ${PREFIX}/bin/${EXE} ]]; then
+            patchelf --replace-needed libgmp.so.3 libgmp.so.10 ${PREFIX}/bin/${EXE}
+            patchelf --replace-needed libffi.so.5 libffi.so.6  ${PREFIX}/bin/${EXE}
+        fi
     done
 elif [[ $(uname) == Darwin ]]; then
     pkgutil --expand pandoc-${PKG_VERSION}.pkg pandoc
